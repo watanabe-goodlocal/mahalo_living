0ff7f935d7d7e2de01978e475d713228044bc663
watanabe-goodlocal <nobuko.watanabe@goodlocal.jp>
2024-11-28 02:47:18 +0900

    Headをlayoutに追加 #1

--- src/nextjs/components/layout.js
+++ src/nextjs/components/layout.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Layout({ children }) {
   return (
     <div className="">
+      <Head>
+        <title>Mahalo Living</title>
+      </Head>
       <main>{children}</main>
     </div>
   );
